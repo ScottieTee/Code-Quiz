1a225acb9dbf3834a2ba2456223c536c324c2789
Sarah Thompson <sthompson@fintechprocessing.com>
2022-02-23 17:04:18 -0500

    added mock questions

--- quiz.js
+++ quiz.js
@@ -14,3 +14,46 @@ const choiceC = document.getElementById("C");
 const choiceD = document.getElementById("D");
 
 const scoreContainer = document.getElementById("scoreContainer");
+
+let questions = [
+    { 
+     question : "loremloremlorem"
+     choiceA : "ipsum"
+     choiceB : "valum"
+     choiceC : "blank"
+     choiceD : "answer"
+     correct : "A"
+     },
+     { 
+        question : "loremloremlorem2"
+        choiceA : "ipsum"
+        choiceB : "valum"
+        choiceC : "blank"
+        choiceD : "answer"
+        correct : "A"
+        },
+     { 
+     question : "loremloremlorem"
+     choiceA : "ipsum"
+     choiceB : "valum"
+     choiceC : "blank"
+     choiceD : "answer"
+     correct : "A"
+     },
+     { 
+        question : "loremloremlorem"
+        choiceA : "ipsum"
+        choiceB : "valum"
+        choiceC : "blank"
+        choiceD : "answer"
+        correct : "A"
+        },
+     { 
+     question : "loremloremlorem"
+     choiceA : "ipsum"
+     choiceB : "valum"
+     choiceC : "blank"
+     choiceD : "answer"
+     correct : "A"
+     },
+]
