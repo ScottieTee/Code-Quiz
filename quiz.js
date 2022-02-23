a12ea58108187813d8d3e5cf28fa9607c7822193
Sarah Thompson <sthompson@fintechprocessing.com>
2022-02-23 16:48:14 -0500

    added JS variables

---
+++ quiz.js
@@ -0,0 +1,16 @@
+const start = document.getElementById("start");
+
+const quiz = document.getElementById("quiz");
+
+const question = document.getElementById("question");
+
+const counter = document.getElementById("counter");
+
+const timeGauge = document.getElementById("timeGauge");
+
+const choiceA = document.getElementById("A");
+const choiceB = document.getElementById("B");
+const choiceC = document.getElementById("C");
+const choiceD = document.getElementById("D");
+
+const scoreContainer = document.getElementById("scoreContainer");
